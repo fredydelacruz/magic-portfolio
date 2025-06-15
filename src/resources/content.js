49b0a42b2601edffb4dfe2ba2a1d8a4148069738
fdelacruzloo7 <fdelacruzloo7@gmail.com>
2025-06-15 11:18:05 +0000

    5

--- src/resources/content.js
+++ src/resources/content.js
@@ -56,11 +56,6 @@ const home = {
   title: `${person.name}`,
   description: `Portfolio website showcasing my work as a ${person.role}`,
   headline: <>Guía Esencial del Rapé Medicinal</>,
-  featured: {
-    display: true,
-    title: <>Recent project: <strong className="ml-4">Once UI</strong></>,
-    href: "/work/building-once-ui-a-customizable-design-system",
-  },
   subline: (
     <>
       El rapé es una medicina sagrada amazónica a base de tabaco y otras plantas, para limpieza energética y la conexión espiritual.
